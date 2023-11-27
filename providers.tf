677569ada4d037897c26961995a6487f951e4aec
vsev2022 <146411173+vsev2022@users.noreply.github.com>
2023-11-27 12:36:19 +0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -19,10 +19,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "b69985b8-36e1-43b4-a5fc-bec3406e0d79"
-  client_id       = "d4bd4432-d40a-4eba-b224-1d9e937c9a3d"
-  client_secret   = "V1e8Q~ZKkpzAhWbgK_.AZ.t_YLfJNYQsEuM8Fc~y"
-  tenant_id       = "fd331275-31f5-402e-9b39-e688905ca73e"
+ 
   features {}
 }
 
